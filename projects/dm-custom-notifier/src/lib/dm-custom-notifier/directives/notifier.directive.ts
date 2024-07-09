4cd6c6af79104e68a03ab8f3d1b8f0e1ad7e4da2
Geronimo-montes <geronimomontes@gmail.com>
2024-07-09 13:00:20 -0700

    Directive: listen click envent, create notification

--- projects/dm-custom-notifier/src/lib/dm-custom-notifier/directives/notifier.directive.ts
+++ projects/dm-custom-notifier/src/lib/dm-custom-notifier/directives/notifier.directive.ts
@@ -1,8 +1,19 @@
-import { Directive } from "@angular/core";
+import { Directive, HostListener, Input } from "@angular/core";
+import { DMNotifierService } from "../services/notifier.service";
+import { DMNotification } from "../models/notification";
 
 @Directive({
-  selector: "[DMNotifier]",
+  selector: "[dmNotifier]",
 })
 export class DMNotifierDirective {
-  constructor() {}
+  constructor(private notifierService: DMNotifierService) {}
+
+  @Input("dmNotifier") notifier?: DMNotification;
+
+  @HostListener("click", ["$event"])
+  onClick(evnet: MouseEvent) {
+    if (this.notifier) {
+      this.notifierService.createNotifier(this.notifier);
+    }
+  }
 }
