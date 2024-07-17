302155048d35869d1b250ae0490b1cc77a216555
Geronimo-montes <geronimomontes@gmail.com>
2024-07-16 19:37:59 -0700

    controls arrows directives

--- projects/dm-game-utils/src/lib/dm-game-utils/components/canvas-game/canvas-game.component.ts
+++ projects/dm-game-utils/src/lib/dm-game-utils/components/canvas-game/canvas-game.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'dm-canvas-game',
-  templateUrl: './canvas-game.component.html',
-  styleUrl: './canvas-game.component.css'
+  selector: "dm-canvas-game",
+  templateUrl: "./canvas-game.component.html",
+  styleUrl: "./canvas-game.component.css",
 })
-export class CanvasGameComponent {
+export class CanvasGameComponent implements AfterViewInit {
+  @ViewChild("canvas") canvas: ElementRef = {} as ElementRef;
+  context2DCanvas: CanvasRenderingContext2D = {} as CanvasRenderingContext2D;
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.context2DCanvas = this.canvas.nativeElement.getContext("2d");
+  }
 }
