b4e56b46866d1404973209da41cf6c8f2dc056ed
Geronimo-montes <geronimomontes@gmail.com>
2024-07-19 13:56:46 -0700

    public-api add export elements

--- projects/dm-game-utils/src/public-api.ts
+++ projects/dm-game-utils/src/public-api.ts
@@ -1,5 +1,19 @@
 /*
  * Public API Surface of dm-game-utils
  */
-
 export { DmGameUtilsModule } from "./lib/dm-game-utils/modules/dm-game-utils.module";
+export * from "./lib/dm-game-utils/components/dm-canvas-game/dm-canvas-game.component";
+export * from "./lib/dm-game-utils/directives/dm-key-arrow-down.directive";
+export * from "./lib/dm-game-utils/directives/dm-key-arrow-up.directive";
+export * from "./lib/dm-game-utils/directives/dm-key-arrow-left.directive";
+export * from "./lib/dm-game-utils/directives/dm-key-arrow-right.directive";
+export * from "./lib/dm-game-utils/errors/dm-render.exception";
+export * from "./lib/dm-game-utils/helpers/dm-render.helper";
+export * from "./lib/dm-game-utils/helpers/dm-colaider.helper";
+export * from "./lib/dm-game-utils/helpers/dm-game-manager.helper";
+export * from "./lib/dm-game-utils/models/dm-colaider.interface";
+export * from "./lib/dm-game-utils/models/dm-game-manager.interface";
+export * from "./lib/dm-game-utils/models/dm-game-manager.model";
+export * from "./lib/dm-game-utils/models/dm-render.interface";
+export * from "./lib/dm-game-utils/pipes/dm-game-utils.pipe";
+export * from "./lib/dm-game-utils/services/dm-game-manager.service";
